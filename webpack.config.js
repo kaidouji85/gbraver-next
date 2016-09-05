2f5438001514f4e795ced0b2f000d03854611efe
kaidouji85 <kaidouji85@gmail.com>
2016-09-06 08:58:47 +0900

    メインシーンを別ファイルに分けた

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
+/**
+ * プロダクションビルドの設定
+ *
+ */
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'index.js',
     path: './public/'
-  }
+  },
+  devtool: 'inline-source-map',
+  cache: true
 };
